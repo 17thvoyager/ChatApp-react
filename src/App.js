45a4092b450a1f974cd70661419c3a5cce78ecfc
Aswin pb <aswinfnx45@gmail.com>
2023-06-24 21:16:37 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useRef } from "react";
+import "./App.css";
+import { Auth } from "./components/Auth";
+
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
 
 function App() {
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
+  const [room, setRoom] = useState(null);
+
+  const roomInputRef = useRef(null);
+
+  if (!isAuth) {
+    return (
+      <div>
+        <Auth setIsAuth={ setIsAuth}/>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {room ? (
+        <div>Chat</div>
+      ) : (
+        <div className="room">
+          <label>Enter Room number</label>
+          <input ref={roomInputRef}/>
+          <button onClick={() => setRoom(roomInputRef.current.value)}>Enter Chat</button>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,7 +1,24 @@
-export const Auth = () => {
+import { auth, provider } from '../firebase-config';
+import { signInWithPopup } from 'firebase/auth'
+
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
+
+export const Auth = (props) => {
+    const signInWithGoogle = async () => {
+        try {
+            const result = await signInWithPopup(auth, provider)
+            cookies.set("auth-token", result.user.refreshToken);    
+            props.setIsAuth(true);
+        } catch(err) {
+            console.log(err)
+        }
+    };
+
     return (
-        <div>
+        <div className="auth">
             <p> click here to sign in </p>
+            <button onClick={signInWithGoogle}>Sign in with google</button>
         </div>
-    )
-}
+    );
+};
